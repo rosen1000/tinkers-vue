fad07d1f21b0ca049a8dc4edcb5ff9e6140871f6
rosen1000 <rosen.ruseff@gmail.com>
2022-01-31 19:20:11 +0200

    utils, more tool handling

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import * as Sentry from '@sentry/vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.mount('#app');
+
+Sentry.init({ app });
